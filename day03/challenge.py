63d07d85e23215a6bd7977feff40f55614b82f89
Rafael Valle <rafael.valle@octoenergy.com>
2022-12-04 09:50:03 +0100

    Challenge 02, Day 03

--- day03/challenge.py
+++ day03/challenge.py
@@ -1,7 +1,9 @@
 import string
+from itertools import zip_longest
 
 
 def priority(item_type: str) -> int:
+    print(f"Item type: {item_type}")
     alphabet = list(string.ascii_lowercase)
     alphabet.extend(list(string.ascii_uppercase))
 
@@ -12,26 +14,31 @@ def priority(item_type: str) -> int:
     return priorities.get(item_type)
 
 
+def grouper(iterable, n, fillvalue=None):
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+
 def main():
     with open("rucksacks.txt") as file:
         lines = list(file)
 
+    rucksacks = [line for line in lines if line != "\n"]
+    rucksacks_by_groups = grouper(rucksacks, 3)
     print("Number of rucksacks: ")
-    print(len([line for line in lines if line != "\n"]))
+    print(len(rucksacks)) 
 
     rucksack_priorities = []
-    for (index, line) in enumerate(lines):
-        if line != "\n":
-            print(f"\nRucksack {index+1}")
-            rucksack = line.rstrip("\n")
-            rucksack_size = len(rucksack)
-            print(f"\nRucksack size: {rucksack_size}")
-            first_compartment, second_compartment = set(rucksack[0:round(rucksack_size/2)]), set(rucksack[round(rucksack_size/2):])
-            common_item_types = first_compartment & second_compartment
-            print(f"\nCommon item types: {common_item_types}")
-            common_item_type_priorities = [priority(item) for item in common_item_types]
-            print(f"Common item types priorities: {common_item_type_priorities}")
-            rucksack_priorities.append(sum(common_item_type_priorities))
+    for (index, line) in enumerate(rucksacks_by_groups):
+        print(f"\nRucksack {index+1}")
+        group_rucksacks = list(line)
+        print("\nGroup rucksacks:")
+        print(group_rucksacks)
+        common_item_types = set(group_rucksacks[0].rstrip("\n")) & set(group_rucksacks[1].rstrip("\n")) & set(group_rucksacks[2].rstrip("\n"))
+        print(f"\nCommon item types: {common_item_types}")
+        common_item_type_priorities = [priority(item) for item in common_item_types]
+        print(f"Common item types priorities: {common_item_type_priorities}")
+        rucksack_priorities.append(sum(common_item_type_priorities))
 
     print(f"Total rucksacks priorities: {sum(rucksack_priorities)}")
 
